9f6c66d79f776d4bf66af2ca11e13a0de1a29c2e
Nguyễn Văn Minh Nhật <95492968+neegandary@users.noreply.github.com>
2024-09-26 19:37:58 +0700

    Update initialization.js

--- js/initialization.js
+++ js/initialization.js
@@ -10,6 +10,87 @@ function createProduct() {
             price: 5000,
             desc: 'Gồm: 4 viên cá'
         },
+        {
+            id: 2,
+            status: 1, 
+            title: 'Bò viên',
+            img: './assets/img/products/bo-vien.jpg',
+            category: 'Đồ chiên',
+            price: 5000,
+            desc: 'Gồm: 4 viên bò'
+        },
+        {
+            id: 3,
+            status: 1, 
+            title: 'Tôm viên',
+            img: './assets/img/products/tom-vien-chien.jpg',
+            category: 'Đồ chiên',
+            price: 5000,
+            desc: 'Gồm: 4 viên tôm'
+        },
+        {
+            id: 4,
+            status: 1, 
+            title: 'Tôm surimi',
+            img: './assets/img/products/tom-surimi.jpg',
+            category: 'Đồ chiên',
+            price: 5000,
+            desc: 'Gồm: 4 viên tôm'
+        },
+        {
+            id: 5,
+            status: 1, 
+            title: 'Hồ lô',
+            img: './assets/img/products/ho-lo.jpg',
+            category: 'Đồ chiên',
+            price: 10000,
+            desc: 'Gồm: 4 viên'
+        },
+        {
+            id: 6,
+            status: 1, 
+            title: 'Đậu hũ phô mai',
+            img: './assets/img/products/dau-hu-pho-mai-0.jpg',
+            category: 'Đồ chiên',
+            price: 5000,
+            desc: 'Gồm: 1 viên'
+        },
+        {
+            id: 7,
+            status: 1, 
+            title: 'Mực xoắn',
+            img: './assets/img/products/muc-xoan.jpg',
+            category: 'Đồ chiên',
+            price: 12000,
+            desc: 'Gồm: 4 viên'
+        },
+        {
+            id: 8,
+            status: 1, 
+            title: 'Cá viên cốm xanh',
+            img: './assets/img/products/ca-vien-com-xanh.jpg',
+            category: 'Đồ chiên',
+            price: 15000,
+            desc: 'Gồm: 4 viên'
+        },
+        {
+            id: 9,
+            status: 1, 
+            title: 'Cá viên mayo',
+            img: './assets/img/products/ca-vien-majo.jpg',
+            category: 'Đồ chiên',
+            price: 15000,
+            desc: 'Gồm: 4 viên'
+        },
+        {
+            id: 10,
+            status: 1, 
+            title: 'Cá viên trứng cút',
+            img: './assets/img/products/cha-ca-trung-cut.jpg',
+            category: 'Đồ chiên',
+            price: 15000,
+            desc: 'Gồm: 4 viên'
+        },
         
         ]
         localStorage.setItem('products', JSON.stringify(products));
